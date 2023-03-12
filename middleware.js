400ea266a075ee0e9febb71d29cdb9e674d5989f
Goldn7799 <syeifsultan@gmail.com>
2023-03-12 16:25:23 +0700

    ready to production

--- middleware.js
+++ middleware.js
@@ -1,4 +1,23 @@
 module.exports = (req, res, next) => {
-  res.header('X-Hello', 'World')
-  next()
+  if(req.url === "request"&&req.method === "GET"){
+    res.sendStatus(403);
+  }else if(req.url === "ping"&&req.method === "GET"){
+    next();
+  }else if(req.url === "users"&&req.method === "GET"){
+    next();
+  }else if(req.url === "post"&&req.method === "GET"){
+    next();
+  }else {
+    if(req.url === "request"&&req.method === "POST"){
+      next();
+    }else if(req.url === "ping"&&req.method === "POST"){
+      res.sendStatus(403);
+    }else if(req.url === "users"&&req.method === "POST"){
+      res.sendStatus(403);
+    }else if(req.url === "post"&&req.method === "POST"){
+      res.sendStatus(403);
+    }else {
+      res.sendStatus(404)
+    }
+  }
 }

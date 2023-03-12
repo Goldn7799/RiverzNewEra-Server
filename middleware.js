5e20a5e9a46f988a6245ff277a6effde84b28aa2
Goldn7799 <syeifsultan@gmail.com>
2023-03-12 17:38:03 +0700

    fixes

--- middleware.js
+++ middleware.js
@@ -1,20 +1,20 @@
 module.exports = (req, res, next) => {
-  if(req.url === "request"&&req.method === "GET"){
+  if(req.url === "/request"&&req.method === "GET"){
     res.sendStatus(403);
-  }else if(req.url === "ping"&&req.method === "GET"){
+  }else if(req.url === "/ping"&&req.method === "GET"){
     next();
-  }else if(req.url === "users"&&req.method === "GET"){
+  }else if(req.url === "/users"&&req.method === "GET"){
     next();
-  }else if(req.url === "post"&&req.method === "GET"){
+  }else if(req.url === "/post"&&req.method === "GET"){
     next();
   }else {
-    if(req.url === "request"&&req.method === "POST"){
+    if(req.url === "/request"&&req.method === "POST"){
       next();
-    }else if(req.url === "ping"&&req.method === "POST"){
+    }else if(req.url === "/ping"&&req.method === "POST"){
       res.sendStatus(403);
-    }else if(req.url === "users"&&req.method === "POST"){
+    }else if(req.url === "/users"&&req.method === "POST"){
       res.sendStatus(403);
-    }else if(req.url === "post"&&req.method === "POST"){
+    }else if(req.url === "/post"&&req.method === "POST"){
       res.sendStatus(403);
     }else {
       res.sendStatus(404)
